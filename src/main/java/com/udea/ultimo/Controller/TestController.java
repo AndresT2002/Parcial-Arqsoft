76398f41643898880bb9d6c6073e96b137c8c9ce
AndresT2002 <73532719+AndresT2002@users.noreply.github.com>
2023-11-28 14:48:14 -0500

    Proyecto inicial

---
+++ src/main/java/com/udea/ultimo/Controller/TestController.java
@@ -0,0 +1,13 @@
+package com.udea.ultimo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping()
+    public String saludo(){
+        return "Hola";
+    }
+
+}
