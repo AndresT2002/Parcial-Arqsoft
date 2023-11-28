e70ecdfc460343f4b11f6d2e28c786cb49794db4
Jair Leal <jair.leal@udea.edu.co>
2023-11-28 16:35:28 -0500

    Paciente

--- src/main/java/com/udea/ultimo/Persistance/Model/Paciente.java
+++ src/main/java/com/udea/ultimo/Persistance/Model/Paciente.java
@@ -1,7 +1,24 @@
 package com.udea.ultimo.Persistance.Model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@Data
+@Table(name = "paciente")
 public class Paciente {
+    @Id
+    @Column(name = "id_paciente")
     private String nombre;
+    @Column(name = "nombre")
     private String apellido;
     private String cedula;
     private String telefono;
@@ -9,5 +26,4 @@ public class Paciente {
     private String correo;
     private String fechaNacimiento;
     private String sexo;
-
 }
