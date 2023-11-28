91ec197842ca465d85a80c7432e4b22ad6efd7ea
Jair Leal <jair.leal@udea.edu.co>
2023-11-28 16:31:56 -0500

    paciente

--- src/main/java/com/udea/ultimo/Persistance/Model/Paciente.java
+++ src/main/java/com/udea/ultimo/Persistance/Model/Paciente.java
@@ -1,4 +1,13 @@
 package com.udea.ultimo.Persistance.Model;
 
 public class Paciente {
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String telefono;
+    private String direccion;
+    private String correo;
+    private String fechaNacimiento;
+    private String sexo;
+
 }
