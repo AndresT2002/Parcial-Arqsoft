7fdf1e728bd83bc6d01088097954406ef7582893
Jair Leal <jair.leal@udea.edu.co>
2023-11-28 16:43:01 -0500

    creacion entity (paciente, medico, historia)

--- src/main/java/com/udea/ultimo/Persistance/Model/HistoriaClinica.java
+++ src/main/java/com/udea/ultimo/Persistance/Model/HistoriaClinica.java
@@ -1,4 +1,39 @@
 package com.udea.ultimo.Persistance.Model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class HistoriaClinica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Integer codigo;
+
+    @Column(name = "nombre_paciente")
+    private String nombrePaciente;
+
+    @Column(name = "cedula_paciente")
+    private String cedulaPaciente;
+
+    @Column(name = "nombre_medico")
+    private String nombreMedico;
+
+    @Column(name = "cedula_medico")
+    private String cedulaMedico;
+
+    @Column(name = "fecha_registro")
+    private Date fechaRegistro;
+
+
 }
