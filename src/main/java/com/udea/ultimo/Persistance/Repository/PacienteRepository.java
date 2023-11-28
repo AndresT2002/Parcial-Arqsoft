6928f0323baf4f6a83da70fac188e4b993a2c3ef
AndresT2002 <73532719+AndresT2002@users.noreply.github.com>
2023-11-28 16:39:12 -0500

    Creacion der servicios y repositorio

--- src/main/java/com/udea/ultimo/Persistance/Repository/PacienteRepository.java
+++ src/main/java/com/udea/ultimo/Persistance/Repository/PacienteRepository.java
@@ -1,4 +1,10 @@
 package com.udea.ultimo.Persistance.Repository;
+import com.udea.ultimo.Persistance.Model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PacienteRepository {
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente,String> {
+
+
 }
